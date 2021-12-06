a51cb93239fb014b201f64db17c7e96f40d21797
sameerrajputra <sameer.rajputra55@gmail.com>
2021-12-07 03:11:58 +0545

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::paginate(2);
+
+        return view('posts.index', [
+            'posts' => $posts
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
+
+        // Post::create([
+        //     'user_id' => auth()->id(),
+        //     'body' => $request->body
+        // ]);
+
+        $request->user()->posts()
+            ->create($request->only('body'));
+        return back();
+    }
+}
